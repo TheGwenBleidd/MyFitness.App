5846c5be16e4a72bc42018a6da353ca6eb7c7a46
Meirlan Arapbay <thegwyenbleidd@gmail.com>
2020-11-05 22:35:38 +0600

    Создана модель Пользователя

---
+++ MyFitness.App/Model/Gender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFitness.App.Model
+{
+    public class Gender
+    {
+        /// <summary>
+        /// Gender
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Checking input
+        /// </summary>
+        /// <param name="name"></param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Error, please enter again" , nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ MyFitness.App/Model/User.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFitness.App.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// Вводим все нужные данные для модели
+        /// </summary>
+
+        #region Properties
+        // <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Возраст.
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public Gender Gender { get;}
+        /// <summary>
+        /// Дата рождения.
+        /// </summary>
+        public DateTime BirthDay { get; }
+        /// <summary>
+        /// Вес.
+        /// </summary>
+        public decimal Weight { get; set; }
+        /// <summary>
+        /// Рост.
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// Создаем констуктор чтоб выводить данные модели 
+        /// </summary>
+        #endregion
+
+        
+        
+        public User(string name,
+                    int age,
+                    Gender gender,
+                    DateTime birthday,
+                    decimal weight,
+                    int height)
+        {
+            
+            #region Check
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name cannot be empty enter again", nameof(name));
+            }
+
+            if(age == 0) 
+            {
+                throw new ArgumentException("Age cannot be 0 , enter again", nameof(age));
+            }
+
+            if(gender == null) 
+            {
+                throw new ArgumentNullException("Gender cannot be null, enter again", nameof(gender));
+            }
+
+            if(birthday < DateTime.Parse("01.01.1910") && birthday >= DateTime.Now ) 
+            {
+                throw new ArgumentException("Birthday cannot be empty , enter again", nameof(birthday));
+            }
+
+            if(weight <= 0) 
+            {
+                throw new ArgumentException("Weight cannot be 0 , enter again" ,nameof(weight));
+            }
+
+            if(height <= 0) 
+            {
+                throw new ArgumentException("Height cannot be 0 , enter again", nameof(height));
+            }
+            #endregion
+            
+            Name = name;
+            Age = age;
+            Gender = gender;
+            BirthDay = birthday;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
